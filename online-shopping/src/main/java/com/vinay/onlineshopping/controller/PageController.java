fd41a43cc2ef84ada0067c52e6aac98cbc58e2b5
vinay kumar <chirukurivinay@gmail.com>
2019-08-14 23:48:13 +0530

    completed the interface theme and other issues

--- online-shopping/src/main/java/com/vinay/onlineshopping/controller/PageController.java
+++ online-shopping/src/main/java/com/vinay/onlineshopping/controller/PageController.java
@@ -9,7 +9,24 @@ public class PageController {
     @RequestMapping(value= { "/","/index","/home"})
 	public ModelAndView index() {
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","welcome spring");
+		mv.addObject("title","home");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
+    
+    @RequestMapping(value="/about")
+   	public ModelAndView about() {
+   		ModelAndView mv=new ModelAndView("page");
+   		mv.addObject("title","About Us");
+   		mv.addObject("userClickAbout",true);
+   		return mv;
+   	}
+    
+    @RequestMapping(value="/contact")
+   	public ModelAndView contact() {
+   		ModelAndView mv=new ModelAndView("page");
+   		mv.addObject("title","Contact Us");
+   		mv.addObject("userClickContact",true);
+   		return mv;
+   	}
 }
---
+++ online-shopping/src/main/webapp/WEB-INF/views/contact.jsp
@@ -0,0 +1,7 @@
+<div class="container">
+  <div class="row">
+    <h1>Contact Us</h1>
+    
+    <p> this is contact page</p>
+  </div>
+</div>
